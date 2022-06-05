b6640ac1aa0cd7ed249a342403599aba88106419
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-06-05 13:59:35 -0300

    Empezando a estructurar y emprolijar como ira toda la informacion (Nada de codeo aun)

---
+++ app.js
@@ -0,0 +1,24 @@
+// Aca van a ir los datos ya planteados para el programa
+
+
+
+
+
+
+
+
+
+// Aca voy a definir los datos que se le van a pedir al usuario para realizar el programa
+
+
+
+
+
+
+
+
+
+
+
+
+// Aca voy a definir las funciones del programa en base a los datos del usuario y los ya planteados

20d5119247163a763c10f5ea452b1fce4c2b9af6
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-06-15 23:51:31 -0300

    Agregando funcion eliminar cliente

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ function Cliente(nombre, apellido, dni, nacionalidad, email, telefono, ){
     this.nacionalidad = nacionalidad;
     this.email = email;
     this.telfono = telefono;
-    
-    
 }
 
 function crearCliente(){
@@ -33,65 +31,74 @@ function crearCliente(){
     let email = prompt("Ingrese su email");
     let telefono = Number ( prompt ("Ingrese su telefono") );
     
-    
     listaClientes.push (new Cliente(nombre, apellido, dni, nacionalidad, email, telefono))
+    console.log("La cantidad de clientes es: " + listaClientes.length);
+    for (let i = 0; i <listaClientes.length; i++){
+        console.log ("Los clientes son los siguientes: " + listaClientes[i]);
+}
+
 
     const cliente = new Cliente(nombre, apellido, dni, nacionalidad, email, telefono);
     return cliente;
-    
 }
 
 const cliente1 = crearCliente()
-const cliente2 = crearCliente()
-
 
 
 console.log(cliente1)
-console.log(cliente2)
-console.log(listaClientes);
 
 
-
-function elegirPagina(){
-    alert("Elige el tipo de pagina que quieres de las siguientes opciones.");
-
-    alert("Los tipos de paginas disponibles son los siguientes: E-commerce -- E-learning -- Estática");
-
-    let tipoPagina = Number ( prompt ( "Escribe 1 = E-commerce, 2 = E-learning y 3 = Estática. Para salir presione ESC" ) );
-
-    while (tipoPagina != "ESC"){
-
-        if (tipoPagina == 1){
-            alert("Usted eligió E-commerce. El precio promedio para una página E-commerce es de: $" + precioEcommerce );
-        }
-        else if (tipoPagina == 2){
-            alert("Usted eligió E-learning. El precio promedio para una página E-learning es de: $" + precioElearning );
-        }
-        else if (tipoPagina == 3){
-            alert("Usted eligió Estática. El precio promedio para una página Estática es de: $" + precioEstatica );
-        }
-        else{
-            alert ("La respuesta es invalida. Vuelva a ingresar una opción");
-        }
-        tipoPagina = Number ( prompt ( "Escribe 1 = E-commerce, 2 = E-learning y 3 = Estática. Para salir presione ESC" ) );
-
+// Esta funcion no la muestro todavia porque quiero que sea tocar un boton. Ya que, no deberia preguntarle al usuario asi por que si
+// Averiguar como se hace por ID
+function eliminarCliente(){
+    let dejaDeSerCliente = prompt("¿Usted desea dejar de ser nuestro cliente?");
+    if (dejaDeSerCliente === "Si"){
+        prompt("¿Podrias comentarnos el por qué? Nos ayudaria mucho para mejorar!");
     }
+    else if (dejaDeSerCliente == "No"){
+        alert("Gracias por seguir confiando en nosotros!");
+    }
+    else{
+        alert("Respuesta invalida ¿Que desea hacer?");
+    }
+    dejaDeSerCliente = prompt("¿Usted desea dejar de ser nuestro cliente?");
+    listaClientes.unshift ()
+    // ¿Que deberia ir entre los parentesis de unshift?
 }
+
 
 // FUNCIONES
 
-
-
-
-
-
-
-// console.log("La cantidad de clientes es: " + listaClientes.length);
-// for (let i = 0; i <listaClientes.length; i++){
-//     console.log ("Los clientes son los siguientes: " + listaClientes[i]);
-// }
+function elegirPagina(){
+    do{
+        alert("Elige el tipo de pagina que quieres de las siguientes opciones.");
+    
+        alert("Los tipos de paginas disponibles son los siguientes: E-commerce -- E-learning -- Estática");
+    
+        let tipoPagina = Number ( prompt ( "Escribe 1 = E-commerce, 2 = E-learning y 3 = Estática. Para salir presione ESC" ) );
+            if (tipoPagina == 1){
+                alert("Usted eligió E-commerce. El precio promedio para una página E-commerce es de: $" + precioEcommerce );
+            }
+            else if (tipoPagina == 2){
+                alert("Usted eligió E-learning. El precio promedio para una página E-learning es de: $" + precioElearning );
+            }
+            else if (tipoPagina == 3){
+                alert("Usted eligió Estática. El precio promedio para una página Estática es de: $" + precioEstatica );
+            }
+            else{
+                alert ("La respuesta es invalida. Vuelva a ingresar una opción");
+            }
+            tipoPagina = Number ( prompt ( "Escribe 1 = E-commerce, 2 = E-learning y 3 = Estática. Para salir presione ESC" ) );
+    }while(tipoPagina != "ESC"){
+    
+    }
+}
 
 elegirPagina();
+
+
+
+
 
 
 

9bdaba0dc6784ce91173bad53e41773d5e628153
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-07-26 21:12:41 -0300

    Dando estilos a productos en el carrito

--- scripts/presupuesto.js
+++ scripts/presupuesto.js
@@ -53,38 +53,24 @@ const agregarProducto = document.getElementsByClassName("btn-agregar")
 
 console.log(agregarProducto)
 
-agregarProducto.addEventListener("click", () => {
 
-    const productoCarrito = document.createElement("div")
-    productoCarrito.classList.add("producto-carrito")
+for(let i = 0 ; i < agregarProducto.length ; i++){
+    
+    agregarProducto[i].addEventListener("click", (e) => {
+        e.preventDefault()
 
-    productoCarrito.innerHTML = `<h4>Página: ${item.nombre}</h4>
-                                <p>Precio: $${item.precio}</p>`
-    productosContainer.push(productoCarrito)
+        const productoCarrito = document.createElement("div")
+        productoCarrito.classList.add("producto-carrito")
+    
+        productoCarrito.innerHTML = `<h4>${Pagina.nombre}</h4>
+                                    <p>Precio $:${Pagina.precio}</p>
+                                    <button class = "eliminar-prod">X</button>`
+        productosContainer.append(productoCarrito)
+    
+        Toastify({
+            text: `Tu producto ${Pagina.nombre} se agregó correctamente`,
+            duration: 2500,
+        }).showToast();
+    }) 
+}
 
-    Toastify({
-        text: `Tu producto ${item.nombre} se agregó correctamente`,
-        duration: 2500,
-    }).showToast();
-}) 
-
-console.log (productoCarrito)
-console.log (productosContainer)
-// Esto no se porque no funciona. Cambie los ID por clases y nada
-// Y tambien acá es donde deberia hacer lo que me comentas sobre el modal que esta siempre
-// oculto y una funcion lo muestra. Puedo empezar aca abajo a hacerlo como creo que seria
-
-
-// function carritoAbierto() {
-//     agregarProducto.addEventListener("click", () => {
-//     })
-// }
-// Aca no entiendo que poner en la funcion. Porque yo lo que quiero es que cuando hago click en el boton, ese elemento se vaya al carrito
-
-
-
-// function carritoCerrado() {
-
-// }
-
-// No se muy bien si tengo que modificar el modal que ya tengo hecho para realizar estas funciones o tengo que plantear el modal de otra manera
